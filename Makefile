e492e7d9eafb979d63408348c87301828254323a
Milica <militsaspasic@gmail.com>
2024-07-02 02:28:07 +0300

    made a makefile, started atoi

--- Makefile
+++ Makefile
@@ -6,8 +6,32 @@
 #    By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/30 16:34:03 by mspasic           #+#    #+#              #
-#    Updated: 2024/06/30 16:58:20 by mspasic          ###   ########.fr        #
+#    Updated: 2024/07/01 12:25:00 by mspasic          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = philo
+
+SOURCES = main.c 
+
+OBJECTS = $(SOURCES:.c=.o)
+
+CC = gcc
+
+FLAGS = -Wall -Wextra -Werror -g -fsanitize=address
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	$(CC) $(FLAGS) -o $(NAME) $(OBJECTS)
+
+clean:
+	rm -f $(OBJECTS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
 
--- main.c
+++ main.c
@@ -6,17 +6,35 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:07:07 by mspasic           #+#    #+#             */
-/*   Updated: 2024/06/30 17:13:34 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/07/02 02:27:18 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //make h file
 #include <stdio.h>
 
+typedef struct s_agora
+{
+	int	philo_num;
+	int	**time_to_die;
+	int	**time_to_eat;
+	int	**time_to_sleep;
+	int	meal_num;
+}	t_agora;
+
+
+int	phil_atoi(int argc, char **argv)
+{
+	
+}
+
 void	start(int argc, char **argv)
 {
+	// int	check;
 
-
+	if (phil_atoi == -1)
+		return (err_invalid);
+}
 
 int	main(int argc, char **argv)
 {
