41a44ebf04527944bdc0a3c5bac90835650ae8fb
SpaMillie <militsaspasic@gmail.com>
2024-07-02 12:54:53 +0300

    made progress in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:07:07 by mspasic           #+#    #+#             */
-/*   Updated: 2024/07/02 02:27:18 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/07/02 12:54:20 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,44 @@ typedef struct s_agora
 	int	**time_to_eat;
 	int	**time_to_sleep;
 	int	meal_num;
+	int	argc;
 }	t_agora;
 
 
-int	phil_atoi(int argc, char **argv)
+int	check_args(t_agora *forum, char **argv)
 {
-	
+	int	i;
+
+	i = 0;
+	while (argv[i] < forum->argc)
+	{
+		if (philo_atoi(argv[i]) == -1)
+			return (-1);
+	}
+
 }
 
-void	start(int argc, char **argv)
+void	start(t_agora *forum, char **argv)
 {
 	// int	check;
 
-	if (phil_atoi == -1)
+
+	if (check_args == -1)
 		return (err_invalid);
 }
 
 int	main(int argc, char **argv)
 {
+	t_agora	forum;
+
+	forum = (t_agora){0};
+	if (argc == 5)
+		forum.argc = 4;
+	else if (argc == 6)
+		forum.argc = 5;
 	if (argc == 5 || argc == 6)
 	{
-		start(argc, argv + 1);
+		start(&forum, argv + 1);
 	}
 	else
 		printf("Woah there! Please give 4 or 5 arguments only!\n");
